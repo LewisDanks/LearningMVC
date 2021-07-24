c05e34a11d521f2172d3f91d8d49316727de812c
LewisDanks <lew.danks@gmail.com>
2021-07-24 21:53:08 +0100

    Test commit

--- ProASPNetCore3/PartyInvites/Controllers/HomeController.cs
+++ ProASPNetCore3/PartyInvites/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PartyInvites.Models;
 
+// Test comment for commit
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
